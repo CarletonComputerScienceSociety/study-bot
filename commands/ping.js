a665c1676e60cf101ed350a8dee496b631bdb8c2
The Drone <38010647+TheDrone7@users.noreply.github.com>
2023-02-02 23:04:53 +0000

    Add roundtrip to ping

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,9 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
+		const startTime = Date.now();
 		await interaction.reply('Pong!');
+		const msg = await interaction.fetchReply();
+		await interaction.editReply(`Pong! (Roundtrip took: ${msg.createdTimestamp - startTime}ms)`);
 	},
 };
